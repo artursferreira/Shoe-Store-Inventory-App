a41e33c72231fd5e1e7f4f37f8bc8c3b3ccd7555
artur <artursferreira27@gmail.com>
2020-11-29 16:59:06 -0300

    Setup Nav Controller in MainActivity.kt

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,17 +3,30 @@ package com.udacity.shoestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+        val navController = findNavController(R.id.navHostFragment)
+        NavigationUI.setupWithNavController(binding.toolbar, navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
